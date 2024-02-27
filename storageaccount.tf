fac9478ed0998d1d916be8c9840ed81299114461
Akshay Sawant <samanta@americas.cshare.net>
2024-02-27 11:13:24 +0000

    Updated storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -3,19 +3,17 @@ resource "random_id" "dns-suffix" {
 }
 
 resource "azurerm_storage_account" "appstore" { 
-  name                     = "cgpprdedasbwk${random_id.dns-suffix.dec}"
+  name                     = "stfabricterraform${random_id.dns-suffix.dec}"
   resource_group_name      = local.resource_group_name
   location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind = "StorageV2"
-  depends_on = [
-    azurerm_resource_group.appgrp
-  ]
+ 
 }
 
 resource "azurerm_storage_container" "data" {
-  for_each = toset(["azure-webjobs-hosts","azure-webjobs-secrets","scm-releases"])
+  for_each = toset(["st-fab-webjobs-hosts","stfab-webjobs-secrets","st-fab-scm-releases"])
   name                  = each.key
   storage_account_name  = azurerm_storage_account.appstore.name
   container_access_type = "private"
