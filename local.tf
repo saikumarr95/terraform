a7ba5668e638d1e6a57f4782384afa87a06a5347
Akshay Sawant <samanta@americas.cshare.net>
2024-02-27 11:05:56 +0000

    Updated local.tf

--- local.tf
+++ local.tf
@@ -1,47 +1,34 @@
 locals {
   tags = {
-    Budget      = "5000"
-    Cost_Center = "CC510"
-    Entity      = "BWK"
-    Environment = "PRODUCTION"
-    Location    = "South Africa North"
-    Owner       = "Cloud Team"
-    Project     = "EDAS"
-    Quota       = "5000"
-    Type        = "MLDATA"
-    Workload    = "EDAS Resources"
+    Client_Facing       = ""
+    Cost_Center         = ""
+    Division            = ""
+    DMZ                 = ""
+    Env                 = ""
+    GBU                 = ""
+    Operational_Hours   = ""
+    Primary_Application = ""
+    Region              = ""
+    Regulation          = ""
+    Support_Owner_Email = ""
+    Type                = ""
+
   }
 }
 
 
 locals {
-  resource_group_name = "TF_CGP_PRD_EDAS_BWK"
-  location            = "South Africa North"
+  resource_group_name = "rg-Fabric-Corp-Terraform-001"
+  location            = "EAST US"
   virtual_network = {
-    name          = "BWK_PRD_VNET_01"
-    address_space = "10.50.0.0/24"
+    name          = "vnet-cis-fabric-p-eus"
+    address_space = "10.204.205.0/24"
   }
 
   subnets = [
     {
       name           = "default"
-      address_prefix = "10.50.0.0/26"
-    },
-    {
-      name           = "Data"
-      address_prefix = "10.50.0.64/26"
-    },
-    {
-      name           = "compute"
-      address_prefix = "10.50.0.160/27"
-    },
-    {
-      name           = "databrickspv"
-      address_prefix = "10.50.0.128/28"
-    },
-    {
-      name           = "databrickspu"
-      address_prefix = "10.50.0.144/28"
+      address_prefix = "10.204.205.0/26"
     }
   ]
 }
