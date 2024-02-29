761f247975bca7d8d838a03ceedc5d8de54e8796
Akshay Sawant <samanta@americas.cshare.net>
2024-02-29 07:24:18 +0000

    Deleted AIOPS

--- AIOPS/aiopswebapp/aiopswebapp/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg-aiops-dev-mvp-eus" {
-  name     = var.resource_group_name
-  location = var.location  
-}
--- AIOPS/azuresearch/azuresearch/output.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  value = azurerm_search_service.search.resource_group_name
-}
-output "location" {
-  value = azurerm_search_service.search.location
-}
-output "azure_search_service_name" {
-  value = azurerm_search_service.search.name
-}
