e683bde9f656774bf68696adaeca23ecc87265f1
Akshay Sawant <samanta@americas.cshare.net>
2024-03-05 13:01:46 +0000

    Updated storageaccount.tf

--- AIOPS_/storageaccount/storageaccount.tf
+++ AIOPS_/storageaccount/storageaccount.tf
@@ -4,4 +4,5 @@ resource "azurerm_storage_account" "staiopsmvp" {
   location                 = var.location
   account_tier             = var.storageacc_account_tier
   account_replication_type = var.storage_account_replication_type
+  public_network_access_enabled = false
 }
