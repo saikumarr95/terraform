15a445f6228c45872cad6f01746f021637659e95
Akshay Sawant <samanta@americas.cshare.net>
2024-03-05 14:32:51 +0000

    Updated storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -3,12 +3,13 @@ resource "random_id" "dns-suffix" {
 }
 
 resource "azurerm_storage_account" "appstore" { 
-  name                     = "stfabricterraform${random_id.dns-suffix.dec}"
+  name                     = "st-fabric-terraform${random_id.dns-suffix.dec}"
   resource_group_name      = local.resource_group_name
   location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind = "StorageV2"
+  public_network_access_enabled = false
  
 }
 
