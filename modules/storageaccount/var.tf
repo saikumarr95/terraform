ede5c34b56c2543f154b0c326e0d4a6dc24e00c6
Akshay Sawant <samanta@americas.cshare.net>
2024-03-07 10:06:27 +0000

    Updated var.tf

---
+++ modules/storageaccount/var.tf
@@ -0,0 +1,22 @@
+variable "resource_group_name" {
+  type=string
+  description = "This defines the resource group name"
+}
+variable "location" {
+  type=string
+  description = "This defines the location of resource"
+}
+
+variable "storageaccount_name" {
+  type=string
+  description = "This defines the service name."
+}
+
+variable "storageacc_account_tier" {
+  type=string
+  description = "This defines the stack name."
+}
+variable "storage_account_replication_type" {
+  type=string
+  description = "This defines the stack version."
+}
