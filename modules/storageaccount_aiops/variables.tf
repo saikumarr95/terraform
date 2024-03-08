48cd886d9c4d93d2d5de04e656f784609aec6701
Akshay Sawant <samanta@americas.cshare.net>
2024-03-08 13:04:09 +0000

    Added variables.tf

---
+++ modules/storageaccount_aiops/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {
+  type=string
+  description = "This defines the resource group name"
+}
+variable "location" {
+  type=string
+  description = "This defines the location of resource"
+}
