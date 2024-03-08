bc7d18e51d1e3c7a9fb67d8a80817a2a8a6c02da
Akshay Sawant <samanta@americas.cshare.net>
2024-03-08 12:40:55 +0000

    Deleted output.tf

--- modules/storageaccount/output.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  value = azurerm_storage_account.staiopsmvp.resource_group_name
-}
-output "location" {
-  value = azurerm_storage_account.staiopsmvp.location
-}
-output "storageaccount_name" {
-  value = azurerm_storage_account.staiopsmvp.name
-}
