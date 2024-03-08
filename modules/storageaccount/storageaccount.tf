98b8f092ec10c39b2f519a2397d5969d68e3d687
Akshay Sawant <samanta@americas.cshare.net>
2024-03-08 12:38:56 +0000

    Deleted storageaccount.tf

--- modules/storageaccount/storageaccount.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "staiopsmvp" {
-  name                     = var.storageaccount_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.storageacc_account_tier
-  account_replication_type = var.storage_account_replication_type
-  public_network_access_enabled = false
-}
