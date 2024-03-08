751bf46e7c7f28ff79316dfde69ca46108976d8b
Akshay Sawant <samanta@americas.cshare.net>
2024-03-08 13:05:25 +0000

    Updated main.tf

--- modules/storageaccount_aiops/main.tf
+++ modules/storageaccount_aiops/main.tf
@@ -1,4 +1,14 @@
-resource "azurerm_resource_group" "rg-aiops-dev-mvp-eus" {
-  name     = var.resource_group_name
-  location = var.location  
+resource "random_id" "dns-suffix" {
+  byte_length = 1
+}
+
+resource "azurerm_storage_account" "appstore" { 
+  name                     = "staiopsterraform${random_id.dns-suffix.dec}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+  public_network_access_enabled = false
+ 
 }
