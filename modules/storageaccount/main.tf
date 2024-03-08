a0280b85f885dda2b7ce486c973d22f7796f3687
Akshay Sawant <samanta@americas.cshare.net>
2024-03-08 11:52:44 +0000

    Updated main.tf

--- modules/storageaccount/main.tf
+++ modules/storageaccount/main.tf
@@ -15,29 +15,3 @@ resource "azurerm_storage_account" "appstore" {
 
 # Create container
 
-resource "azurerm_storage_container" "data" {
-  for_each = toset(["st-fab-webjobs-hosts","stfab-webjobs-secrets","st-fab-scm-releases"])
-  name                  = each.key
-  storage_account_name  = azurerm_storage_account.appstore.name
-  container_access_type = "blob"
-  depends_on = [
-    azurerm_storage_account.appstore
-  ]
-}
-/*
-resource "azurerm_storage_blob" "files" {
-  for_each = {
-    sample1="C:\\tmp\\sample1.txt"
-    sample2="C:\\tmp\\sample2.txt"
-    sample3="C:\\tmp\\sample3.txt"
-  }
-  name                   = each.key
-  storage_account_name   = azurerm_storage_account.appstore.name
-  storage_container_name = "data"
-  type                   = "Block"
-  source                 = each.value
-  depends_on = [
-    azurerm_storage_account.appstore
-  ]
-}
-*/
