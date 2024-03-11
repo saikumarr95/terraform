aaf24ee1680154b102814142f2b091e0a83e5741
Ebin Thomas <thomase2@americas.cshare.net>
2024-03-11 06:30:01 +0000

    Updated main.tf

--- modules/aiopswebapp/main.tf
+++ modules/aiopswebapp/main.tf
@@ -1,4 +1,5 @@
-resource "azurerm_resource_group" "rg-AIOps-Corp-Connectivity-eus" {
+/*resource "azurerm_resource_group" "rg-AIOps-Corp-Connectivity-eus" {
   name     = var.resource_group_name
   location = var.location  
 }
+*/
