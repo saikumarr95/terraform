992ffed612d20d44b5f16ae4899fc05181a86259
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 12:47:21 +0000

    Updated main_aiops.tf

--- main_aiops.tf
+++ main_aiops.tf
@@ -23,9 +23,11 @@ module "azure_search_module" {
    location = "eastus"
    azure_search_service_name = "as-aiops-mvp-eus"
 }
+*/
+
 module "azure_openai_module" {
 source = "./modules/azureopenai"
-resource_group_name = "rg-aiops-mvp-eus"
+resource_group_name = "rg-AIOps-Corp-Connectivity-eus"
 location = "eastus"
 azure_openai_service_name = "azopenai-aiops-mvp-eus"
 azure_openai_deployment_name = "azopenai-deployment-aiops-mvp-eus"
