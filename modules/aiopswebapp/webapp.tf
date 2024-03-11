9c7e43a8ca86f31e54da4ca1328bee72ba010318
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 08:24:44 +0000

    Updated webapp.tf

--- modules/aiopswebapp/webapp.tf
+++ modules/aiopswebapp/webapp.tf
@@ -1,4 +1,4 @@
-
+/*
 resource "azurerm_service_plan" "aiopswebapp_service_plan" {
   name                = var.aiops_serviceplan_name
   resource_group_name = var.resource_group_name
@@ -9,7 +9,7 @@ resource "azurerm_service_plan" "aiopswebapp_service_plan" {
  #   azurerm_resource_group.rg-AIOps-Corp-Connectivity-eus
  # ]
 }
-/*
+
 resource "azurerm_windows_web_app" "aiops_webapp" {
   name                = var.aiops_webapp_name
   resource_group_name = var.resource_group_name
@@ -29,11 +29,23 @@ resource "azurerm_windows_web_app" "aiops_webapp" {
 }
 */
 ##
-resource "azurerm_app_service" "aiops_webapp" {  
-  name                = var.aiops_webapp_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  service_plan_id     = azurerm_service_plan.aiopswebapp_service_plan.id
+
+resource "azurerm_app_service_plan" "aiops_app_service_plan" {  
+  name                = var.aiops_serviceplan_name  
+  resource_group_name = var.resource_group_name  
+  location            = var.location  
+  sku {  
+    tier = "Basic"  
+    size = "B1"  
+  }  
+}  
+
+resource "azurerm_app_service" "aiops_app" {  
+  name                = var.aiops_webapp_name  
+  resource_group_name = var.resource_group_name  
+  location            = var.location  
+  app_service_plan_id = azurerm_app_service_plan.aiops_app_service_plan.id  
+  
   site_config {  
     app_command_line = "python manage.py runserver 0.0.0.0:8000"  
     linux_fx_version = "PYTHON|3.8"  
@@ -43,7 +55,7 @@ resource "azurerm_app_service" "aiops_webapp" {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"  
   }  
   
-  depends_on = [
-    azurerm_service_plan.aiopswebapp_service_plan
-  ]
+  depends_on = [  
+    azurerm_app_service_plan.aiops_app_service_plan  
+  ]  
 }  
