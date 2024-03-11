34ddb3ff1b5b47145cbe364b54fbb7c316ba1a3b
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 10:38:41 +0000

    Updated output.tf

--- modules/aiopswebapp/output.tf
+++ modules/aiopswebapp/output.tf
@@ -5,13 +5,15 @@ output "resource_group_name" {
 output "location" {
   value = azurerm_service_plan.aiopswebapp_service_plan.location
 }
+
+
 output "azure_service_plan_name" {
   value = azurerm_service_plan.aiopswebapp_service_plan.name
 }
 output "azure_webapp_name" {
-  value = azurerm_windows_web_app.aiops_webapp.name
+  value = azurerm_linux_web_app.aiops_webapp.name
 }
-*/
+
 output "app_service_plan_id" {  
   value = azurerm_service_plan.aiops_app_service_plan.id  
 }  
@@ -23,3 +25,4 @@ output "web_app_id" {
 output "web_app_default_hostname" {  
   value = azurerm_service.aiops_app.default_site_hostname  
 }  
+*/
