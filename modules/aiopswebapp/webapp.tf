5e517f103f533861cbbd12f2882253a056e4102a
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 08:18:59 +0000

    Updated webapp.tf

--- modules/aiopswebapp/webapp.tf
+++ modules/aiopswebapp/webapp.tf
@@ -9,7 +9,7 @@ resource "azurerm_service_plan" "aiopswebapp_service_plan" {
  #   azurerm_resource_group.rg-AIOps-Corp-Connectivity-eus
  # ]
 }
-
+/*
 resource "azurerm_windows_web_app" "aiops_webapp" {
   name                = var.aiops_webapp_name
   resource_group_name = var.resource_group_name
@@ -27,3 +27,23 @@ resource "azurerm_windows_web_app" "aiops_webapp" {
     azurerm_service_plan.aiopswebapp_service_plan
   ]
 }
+*/
+##
+resource "azurerm_app_service" "aiops_webapp" {  
+  name                = var.aiops_webapp_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.aiopswebapp_service_plan.id
+  site_config {  
+    app_command_line = "python manage.py runserver 0.0.0.0:8000"  
+    linux_fx_version = "PYTHON|3.8"  
+  }  
+  
+  app_settings = {  
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"  
+  }  
+  
+  depends_on = [
+    azurerm_service_plan.aiopswebapp_service_plan
+  ]
+}  
