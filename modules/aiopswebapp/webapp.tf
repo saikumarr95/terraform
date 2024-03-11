7ce9703e6c5fc35b5a47f83d698778d9aa98adbe
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 10:35:39 +0000

    Updated webapp.tf

--- modules/aiopswebapp/webapp.tf
+++ modules/aiopswebapp/webapp.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "azurerm_service_plan" "aiopswebapp_service_plan" {
   name                = var.aiops_serviceplan_name
   resource_group_name = var.resource_group_name
@@ -10,26 +10,26 @@ resource "azurerm_service_plan" "aiopswebapp_service_plan" {
  # ]
 }
 
-resource "azurerm_windows_web_app" "aiops_webapp" {
+resource "azurerm_linux_web_app" "aiops_webapp" {
   name                = var.aiops_webapp_name
   resource_group_name = var.resource_group_name
   location            = var.location
   service_plan_id     = azurerm_service_plan.aiopswebapp_service_plan.id
-
+  https_only          = true
+  public_network_access_enabled = false
   site_config {
-    application_stack {
-      current_stack=var.current_stack
-      dotnet_version= var.dotnet_version
-    }
+   # application_stack {
+   #   current_stack=var.current_stack
+   #   dotnet_version= var.dotnet_version
+   # }
   }
 
   depends_on = [
     azurerm_service_plan.aiopswebapp_service_plan
   ]
 }
-*/
 ##
-
+/*
 #resource "azurerm_app_service_plan" "aiops_app_service_plan" {  
 resource "azurerm_service_plan" "aiops_app_service_plan" {  
   name                = var.aiops_serviceplan_name  
@@ -65,3 +65,4 @@ resource "azurerm_linux_web_app" "aiops_app" {
     azurerm_service_plan.aiops_app_service_plan  
   ]  
 }  
+*/
