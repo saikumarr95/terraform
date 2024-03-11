68579af03a7a77f062d5e1569deabb6ad44e85c4
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 15:01:45 +0000

    Updated azurefunction.tf

--- modules/azurefunction/azurefunction.tf
+++ modules/azurefunction/azurefunction.tf
@@ -1,4 +1,35 @@
+resource "azurerm_storage_account" "function-appserviceplan" {
+  name                     = "fa-aiops-terraform-001"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
+resource "azurerm_service_plan" "function-appserviceplan" {
+  name                = "example-app-service-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "" {
+  name                = "example-linux-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
+
+
+
+
+/*
 
 resource "azurerm_app_service_plan" "function-appserviceplan" {
   name                = var.app_function_serviceplan_name
@@ -26,3 +57,4 @@ resource "azurerm_function_app" "function_name" {
   storage_account_name       = azurerm_storage_account.aiops-storage.name
   storage_account_access_key = azurerm_storage_account.aiops-storage.primary_access_key
 }
+*/
