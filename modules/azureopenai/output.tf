485188e68c87b36f80679daaaea3da6228602905
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 13:07:49 +0000

    Updated output.tf

--- modules/azureopenai/output.tf
+++ modules/azureopenai/output.tf
@@ -1,3 +1,23 @@
+output "ai_account_endpoint" {  
+  value = azurerm_cognitive_account.ai_account.endpoint  
+}  
+output "ai_account_key" {  
+  value = azurerm_cognitive_account.ai_account.key  
+}  
+output "openai_endpoint" {  
+  value = azurerm_cognitive_account_openai.openai.endpoint  
+}  
+output "openai_key" {  
+  value = azurerm_cognitive_account_openai.openai.key  
+}  
+output "ai_search_endpoint" {  
+  value = azurerm_cognitive_account_search.ai_search.endpoint  
+}  
+output "ai_search_key" {  
+  value = azurerm_cognitive_account_search.ai_search.key  
+}  
+
+/*
 output "openai_account_id" {  
   value = azurerm_cognitive_services_account.aiops_openai.id  
 }  
@@ -13,7 +33,7 @@ output "openai_account_endpoint" {
 output "openai_account_key" {  
   value = azurerm_cognitive_services_account.aiops_openai.primary_access_key  
 }  
-/*
+
 
 output "resource_group_name" {
   value = azurerm_cognitive_account.azure_open_ai.resource_group_name
