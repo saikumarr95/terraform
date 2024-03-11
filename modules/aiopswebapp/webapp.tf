a6cb4fdc9f4bd90b7e2bd4b454be2971c1bf841d
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 10:48:47 +0000

    Updated webapp.tf

--- modules/aiopswebapp/webapp.tf
+++ modules/aiopswebapp/webapp.tf
@@ -4,7 +4,7 @@ resource "azurerm_service_plan" "aiopswebapp_service_plan" {
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = var.os_type
-  sku_name            = var.sku_name
+  sku_name            = "P1v3"
  # depends_on = [
  #   azurerm_resource_group.rg-AIOps-Corp-Connectivity-eus
  # ]
