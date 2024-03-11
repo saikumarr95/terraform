96eec97930f1e2f7aef182b42b661c1cd0b043e7
Akshay Sawant <samanta@americas.cshare.net>
2024-03-11 13:06:17 +0000

    Updated azureopenaiscript.tf

--- modules/azureopenai/azureopenaiscript.tf
+++ modules/azureopenai/azureopenaiscript.tf
@@ -1,29 +1,34 @@
-  
-resource "azurerm_cognitive_services_account" "aiops_openai" {  
-  name                = var.aiops_openai_name  
-  resource_group_name = var.resource_group_name  
-  location            = var.location  
+
+resource "azurerm_cognitive_account" "ai_account" {  
+  name                = var.ai_account_name  
+  resource_group_name = azurerm_resource_group.rg.name  
   kind                = "CognitiveServices"  
-  sku_name            = "S0"  
-  sku_tier            = "Standard"  
-  sku_capacity        = 1  
-  
+  sku_name            = var.ai_account_sku  
   identity {  
     type = "SystemAssigned"  
   }  
-  
-  custom_subdomain_name = var.aiops_custom_subdomain_name  
-  
-  properties = jsonencode({  
-    apiProperties = {  
-      "model" = "gpt3-turing-5"  
-    }  
-  })  
-    
+}  
+resource "azurerm_cognitive_account_openai" "openai" {  
+  name                = var.openai_name  
+  resource_group_name = azurerm_resource_group.rg.name  
+  kind                = "OpenAi"  
+  sku_name            = var.openai_sku  
+  identity {  
+    type = "SystemAssigned"  
+  }  
+}  
+resource "azurerm_cognitive_account_search" "ai_search" {  
+  name                = var.ai_search_name  
+  resource_group_name = azurerm_resource_group.rg.name  
+  kind                = "AzureSearch"  
+  sku_name            = var.ai_search_sku  
+  identity {  
+    type = "SystemAssigned"  
+  }  
 }  
 
-
 /*
+
 resource "azurerm_cognitive_account" "azure_open_ai" {
   name = var.azure_openai_service_name
   location =var.location
@@ -32,6 +37,7 @@ resource "azurerm_cognitive_account" "azure_open_ai" {
   sku_name = var.sku_name
   public_network_access_enabled = false
   }
+
 
 resource "azurerm_cognitive_deployment" "azure_open_ai_deployment" {
   name = var.azure_openai_deployment_name
