c83ec5f9a713fb4d296f6218101c686291aba59b
Akshay Sawant <samanta@americas.cshare.net>
2024-03-12 11:17:04 +0000

    Updated main.tf

--- modules/storageaccount_aiops/main.tf
+++ modules/storageaccount_aiops/main.tf
@@ -12,3 +12,33 @@ resource "azurerm_storage_account" "appstore" {
   public_network_access_enabled = false
  
 }
+
+# Private Endpoint
+resource "azurerm_private_dns_zone" "storage_account_dns_zone" {  
+  name                = var.private_dns_zone_name  
+  location            = azurerm_resource_group.storage_rg.location  
+  resource_group_name = azurerm_resource_group.storage_rg.name  
+}  
+resource "azurerm_private_dns_a_record" "storage_account_dns_record" {  
+  name                = azurerm_storage_account.storage_account.name  
+  zone_name           = azurerm_private_dns_zone.storage_account_dns_zone.name  
+  resource_group_name = azurerm_resource_group.storage_rg.name  
+  ttl                 = 300  
+  records             = [azurerm_storage_account.storage_account.primary_blob_endpoint]  
+}  
+resource "azurerm_private_endpoint" "storage_account_private_endpoint" {  
+  name                = var.private_endpoint_name  
+  location            = azurerm_resource_group.storage_rg.location  
+  resource_group_name = azurerm_resource_group.storage_rg.name  
+  subnet_id           = var.subnet_id  
+  private_service_connection {  
+    name                           = "storageaccount-private-connection"  
+    private_connection_resource_id = azurerm_storage_account.storage_account.id  
+    is_manual_connection           = false  
+    subresource_names              = ["blob"]  
+  }  
+  dns_zone_group {  
+    name         = azurerm_private_dns_zone.storage_account_dns_zone.name  
+    private_dns_zone_id = azurerm_private_dns_zone.storage_account_dns_zone.id  
+  }  
+}  
