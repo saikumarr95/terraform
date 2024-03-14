7f918ad36d22ed5d7b478aeb3866be000d9aafe1
Akshay Sawant <samanta@americas.cshare.net>
2024-03-14 11:24:01 +0000

    Updated output.tf

--- modules/storageaccount_aiops/output.tf
+++ modules/storageaccount_aiops/output.tf
@@ -1,9 +1,7 @@
 output "private_endpoint_id" {  
   value = azurerm_private_endpoint.storage_account_private_endpoint.id  
 }  
-#output "private_dns_zone_id" {  
-#  value = azurerm_private_dns_zone.storage_account_dns_zone.id  
-#} 
+
 # Fetch subnet_id
 output "virtualnetwork_subnets_ids" {
   value = data.azurerm_subnet.virtualSubnets1.*.id
