60e792e052f5e15bec1ddeb7fc56d9e5a79b6e75
Akshay Sawant <samanta@americas.cshare.net>
2024-03-20 09:52:35 +0000

    Updated azuresearchscript.tf

--- modules/azuresearch/azuresearchscript.tf
+++ modules/azuresearch/azuresearchscript.tf
@@ -3,4 +3,11 @@ resource "azurerm_search_service" "search" {
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = var.sku
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
 }
