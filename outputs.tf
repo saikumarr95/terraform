e4b87b08da996da9501badae664d7c87ced345a7
Akshay Sawant <samanta@americas.cshare.net>
2024-03-22 12:48:37 +0000

    Updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 output "storage_account_name" {  
   value = module.azure_storageaccount_module.storage_account_name  
 }  
-  
+/*  
 # Outputs for azure_search_module  
 output "search_service_name" {  
   value = module.azure_search_module.search_service_name  
@@ -13,7 +13,7 @@ output "search_service_name" {
 output "openai_service_endpoint" {  
   value = module.azure_openai_module.openai_service_endpoint4  
 }  
-/*  
+  
 # Outputs for azure_webapp_module  
 output "webapp_url" {  
   value = module.azure_webapp_module.webapp_url  
