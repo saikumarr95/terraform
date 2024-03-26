56fd9bd70023689e61970c29c839f51d62ac283e
Ebin Thomas <thomase2@americas.cshare.net>
2024-03-26 13:31:39 +0000

    Deleted data.tf

--- AiOps/modules/private-endpoint/data.tf
+++
@@ -1,5 +0,0 @@
-data "azurerm_subnet" "virtualSubnets1" {
-  name                 = "sn-staiopsstorage-eus"
-  virtual_network_name = "vnet-cis-aiops-d-eus"
-  resource_group_name  = var.resource_group_name
-}
