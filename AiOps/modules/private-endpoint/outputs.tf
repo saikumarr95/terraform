174e27b3b2219ab3193a241639045b8f1405dc17
Ebin Thomas <thomase2@americas.cshare.net>
2024-03-26 13:31:52 +0000

    Deleted outputs.tf

--- AiOps/modules/private-endpoint/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "private_endpoint_id" {  
-  value = azurerm_private_endpoint.storage_account_private_endpoint.id  
-}  
-
-output "virtualnetwork_subnets_ids" {
-  value = data.azurerm_subnet.virtualSubnets1.*.id
-}
