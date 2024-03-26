fa02d5b11807861d90e58960fddb1488cf7c93bb
Ebin Thomas <thomase2@americas.cshare.net>
2024-03-26 11:40:58 +0000

    Updated variables.tf

---
+++ AiOps/variables.tf
@@ -0,0 +1,24 @@
+variable "aiops_serviceplan_name" {
+  description = "The name of the service plan"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resources"
+  type        = string
+}
+
+variable "os_type" {
+  description = "The OS type of the service plan"
+  type        = string
+}
+
+variable "aiops_webapp_name" {
+  description = "The name of the web app"
+  type        = string
+}
