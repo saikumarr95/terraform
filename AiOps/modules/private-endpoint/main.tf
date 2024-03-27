1c7c3b650090cb86367e4defd0d68c28b3ce4816
Ebin Thomas <thomase2@americas.cshare.net>
2024-03-27 10:28:20 +0000

    Added main.tf

---
+++ AiOps/modules/private-endpoint/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_private_endpoint" "generic_private_endpoint" {
+  name                = var.private_endpoint_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = var.private_service_connection
+    private_connection_resource_id = var.resource_id
+    is_manual_connection           = false
+    subresource_names              = [var.subresource_name]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags, 
+      private_dns_zone_group,
+    ]
+  }
+}
