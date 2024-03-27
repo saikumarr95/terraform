6c29f30ffde7c1f4f7f505ecd748aa27c574d250
Ebin Thomas <thomase2@americas.cshare.net>
2024-03-27 10:33:39 +0000

    Updated main.tf

--- AiOps/main.tf
+++ AiOps/main.tf
@@ -9,3 +9,15 @@ module "azure_webapp" {
   common_tags            = var.common_tags
   ip_restrictions        = var.ip_restrictions
 }
+
+module "private_endpoint" {
+  source = "./modules/private_endpoint"
+
+  private_endpoint_name       = var.private_endpoint_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  subnet_id                   = var.subnet_id
+  private_service_connection  = var.private_service_connection
+  resource_id                 = azurerm_storage_account.appstore.id
+  subresource_name            = "blob"
+}
