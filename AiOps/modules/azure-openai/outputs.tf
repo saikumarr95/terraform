ba382f13d39f3fb6ba79a56b392c5101f26af3b9
Ebin Thomas <thomase2@americas.cshare.net>
2024-04-02 09:38:00 +0000

    Added outputs.tf

---
+++ AiOps/modules/azure-openai/outputs.tf
@@ -0,0 +1,53 @@
+/*
+output "ai_account_endpoint" {  
+  value = azurerm_cognitive_account.ai_account.endpoint  
+}  
+output "ai_account_key" {  
+  value = azurerm_cognitive_account.ai_account.key  
+}  
+output "openai_endpoint" {  
+  value = azurerm_cognitive_account_openai.openai.endpoint  
+}  
+output "openai_key" {  
+  value = azurerm_cognitive_account_openai.openai.key  
+}  
+output "ai_search_endpoint" {  
+  value = azurerm_cognitive_account_search.ai_search.endpoint  
+}  
+output "ai_search_key" {  
+  value = azurerm_cognitive_account_search.ai_search.key  
+}  
+
+
+output "openai_account_id" {  
+  value = azurerm_cognitive_services_account.aiops_openai.id  
+}  
+  
+output "openai_account_name" {  
+  value = azurerm_cognitive_services_account.aiops_openai.name  
+}  
+  
+output "openai_account_endpoint" {  
+  value = azurerm_cognitive_services_account.aiops_openai.endpoint  
+}  
+  
+output "openai_account_key" {  
+  value = azurerm_cognitive_services_account.aiops_openai.primary_access_key  
+}  
+
+
+output "resource_group_name" {
+  value = azurerm_cognitive_account.azure_open_ai.resource_group_name
+}
+output "location" {
+  value = azurerm_cognitive_account.azure_open_ai.location
+}
+
+
+output "azure_openai_service_name" {
+  value = azurerm_cognitive_account.azure_open_ai.name
+}
+output "azure_openai_deployment_name" {
+  value = azurerm_cognitive_deployment.azure_open_ai_deployment.name
+}
+*/
