3bddd82de6fbd12d7dc7c9a9b787b133347e48bc
Ebin Thomas <thomase2@americas.cshare.net>
2024-04-03 10:01:44 +0000

    Updated variables.tf

--- AiOps/modules/azure-search/variables.tf
+++ AiOps/modules/azure-search/variables.tf
@@ -1,21 +1,14 @@
-variable "resource_group_name" {
-  type=string
-  description = "This defines the Azure search resource group name"
-}
-variable "location" {
-  type=string
-  description = "This defines the Azure search location of resource"
-}
-variable "azure_search_service_name" {
-  type=string
-  description = "This defines the Azure search service name."
-}
-variable "sku" {
-  description = "The pricing tier of the search service you want to create (for example, basic or standard)."
-  default     = "standard"
-  type        = string
+
+variable "azure_search" {
+  type = object({
+    resource_group_name = string
+    location = string
+    service_name = string
+    sku = string
+  })
+  description = "This defines the Azure search configuration."
   validation {
-    condition     = contains(["free", "basic", "standard", "standard2", "standard3", "storage_optimized_l1", "storage_optimized_l2"], var.sku)
+    condition     = contains(["free", "basic", "standard", "standard2", "standard3", "storage_optimized_l1", "storage_optimized_l2"], var.azure_search.sku)
     error_message = "The sku must be one of the following values: free, basic, standard, standard2, standard3, storage_optimized_l1, storage_optimized_l2."
   }
 }
