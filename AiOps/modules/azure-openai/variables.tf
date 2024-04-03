a7b22a10ca153d865a6d43ccb98d30a0d1abe301
Ebin Thomas <thomase2@americas.cshare.net>
2024-04-03 10:05:24 +0000

    Updated variables.tf

--- AiOps/modules/azure-openai/variables.tf
+++ AiOps/modules/azure-openai/variables.tf
@@ -1,29 +1,11 @@
-variable "azure_openai_service_name" {
-  description = "The name of the Azure OpenAI service"
-  type        = string
-}
-
-variable "location" {
-  description = "The location where the Azure OpenAI service should be created"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the Azure OpenAI service"
-  type        = string
-}
-
-variable "azure_openai_kind" {
-  description = "The kind of the Azure OpenAI service"
-  type        = string
-}
-
-variable "sku_name" {
-  description = "The SKU name for the Azure OpenAI service"
-  type        = string
-}
-
-variable "azure_openai_deployment_name" {
-  description = "The name of the Azure OpenAI deployment"
-  type        = string
+variable "azure_openai" {
+  type = object({
+    service_name = string
+    location = string
+    resource_group_name = string
+    kind = string
+    sku_name = string
+    deployment_name = string
+  })
+  description = "This defines the Azure OpenAI configuration."
 }
