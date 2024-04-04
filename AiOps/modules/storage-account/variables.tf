f995f20028ef3676337c7f08220ada83c0bca3e2
Ebin Thomas <thomase2@americas.cshare.net>
2024-04-04 06:34:29 +0000

    Updated variables.tf

--- AiOps/modules/storage-account/variables.tf
+++ AiOps/modules/storage-account/variables.tf
@@ -1,25 +1,13 @@
-variable "azure_resource_name" {
-  description = "The name of the Azure resource to include in the storage account name."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the storage account."
-  type        = string
-}
-
-variable "location" {
-  description = "The location/region in which to create the storage account."
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "The ID of the subnet for the storage account."
-  type        = string
-}
-
-variable "environment" {
-  description = "The environment for the storage account."
-  type        = string
-  default     = "development"
+variable "storage_account" {
+  type = object({
+    azure_resource_name = string
+    resource_group_name = string
+    location = string
+    subnet_id = string
+    environment = string
+  })
+  description = "This defines the Azure Storage Account configuration."
+  default = {
+    environment = "development"
+  }
 }
