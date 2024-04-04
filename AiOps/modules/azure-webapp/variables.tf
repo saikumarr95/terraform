8b31b262cfac5aca191e2f4b600ef9a69a3d993f
Ebin Thomas <thomase2@americas.cshare.net>
2024-04-04 06:27:58 +0000

    Updated variables.tf

--- AiOps/modules/azure-webapp/variables.tf
+++ AiOps/modules/azure-webapp/variables.tf
@@ -1,39 +1,19 @@
-variable "aiops_serviceplan_name" {
-  description = "The name of the service plan"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "The location of the resources"
-  type        = string
-}
-
-variable "os_type" {
-  description = "The OS type of the service plan"
-  type        = string
-}
-
-variable "aiops_webapp_name" {
-  description = "The name of the web app"
-  type        = string
-}
-
-variable "common_tags" {
-  description = "Common tags to be applied to resources"
-  type        = map(string)
-  default     = {
-    ManagedBy   = "Terraform"
-    Environment = "Production"
+variable "aiops" {
+  type = object({
+    serviceplan_name = string
+    resource_group_name = string
+    location = string
+    os_type = string
+    webapp_name = string
+    common_tags = map(string)
+    ip_restrictions = list(map(string))
+  })
+  description = "This defines the AIOPS configuration."
+  default = {
+    common_tags = {
+      ManagedBy   = "Terraform"
+      Environment = "Production"
+    }
+    ip_restrictions = []
   }
-}
-
-variable "ip_restrictions" {
-  description = "IP restrictions for the web app"
-  type        = list(map(string))
-  default     = []
 }
