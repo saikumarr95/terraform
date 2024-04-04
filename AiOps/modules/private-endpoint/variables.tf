1d694b2dd6219da5c68b88fe190e4d3b72e743e8
Ebin Thomas <thomase2@americas.cshare.net>
2024-04-04 06:30:12 +0000

    Updated variables.tf

--- AiOps/modules/private-endpoint/variables.tf
+++ AiOps/modules/private-endpoint/variables.tf
@@ -1,34 +1,12 @@
-variable "private_endpoint_name" {
-  description = "The name of the private endpoint."
-  type        = string
-}
-
-variable "location" {
-  description = "The location of the private endpoint."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the private endpoint."
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "The ID of the Subnet in which to create the private endpoint."
-  type        = string
-}
-
-variable "private_service_connection" {
-  description = "The name of the private service connection."
-  type        = string
-}
-
-variable "resource_id" {
-  description = "The resource ID of the resource for which a private endpoint is being created."
-  type        = string
-}
-
-variable "subresource_name" {
-  description = "The subresource name which the Private Endpoint is to be connected with."
-  type        = string
+variable "private_endpoint" {
+  type = object({
+    name = string
+    location = string
+    resource_group_name = string
+    subnet_id = string
+    private_service_connection = string
+    resource_id = string
+    subresource_name = string
+  })
+  description = "This defines the Azure Private Endpoint configuration."
 }
