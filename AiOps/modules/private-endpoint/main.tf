9e5119bb0fec5e074d44a9ea74672e95f2668469
Ebin Thomas <thomase2@americas.cshare.net>
2024-04-09 14:43:47 +0000

    Updated main.tf

--- AiOps/modules/private-endpoint/main.tf
+++ AiOps/modules/private-endpoint/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_private_endpoint" "generic_private_endpoint" {
   name                = var.private_endpoint_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
+  subnet_id           = flatten(data.azurerm_subnet.virtualSubnets1.*.id)[0] 
 
   private_service_connection {
     name                           = var.private_service_connection
